71f598f7f7bab5b0e06c5fc97d2165e21c7bd497
LostOxygen <jonathan.evertz@gmail.com>
2021-03-23 18:50:24 +0100

    data structure changes and display kanas

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from PyKanaTrainer.trainer import KanaTrainer
+import unicodedata
 from curses import wrapper
-from typing import Union
 import curses
 import random
 import time
@@ -12,7 +12,9 @@ import sys
 def start(stdscr) -> None:
     #initialize the program and draw the standard screen
     win, rows, cols = initialize_screen()
+    rows, cols = rows-1, cols-1
     draw_standard_screen(win, rows, cols)
+    trainer = KanaTrainer()
     train_method = 0
 
     #startscreen menu loop
@@ -33,23 +35,41 @@ def start(stdscr) -> None:
             break
         #display the training methods and their highlighting
         win.addstr(int(rows/2)-10, int(cols/2)-11, "Choose Training Method:", curses.A_UNDERLINE)
-        win.addstr(int(rows/2)-8, int(cols/2)-12, "[ 1 ] Hiragana "+u"\u2192"+" Romaji", curses.A_REVERSE if train_method is 0 else curses.A_NORMAL)
-        win.addstr(int(rows/2)-6, int(cols/2)-12, "[ 2 ] Katakana "+u"\u2192"+" Romaji", curses.A_REVERSE if train_method is 1 else curses.A_NORMAL)
-        win.addstr(int(rows/2)-4, int(cols/2)-12, "[ 3 ] Romaji "+u"\u2192"+" Hiragana", curses.A_REVERSE if train_method is 2 else curses.A_NORMAL)
-        win.addstr(int(rows/2)-2, int(cols/2)-12, "[ 4 ] Romaji "+u"\u2192"+" Katakana", curses.A_REVERSE if train_method is 3 else curses.A_NORMAL)
+        win.addstr(int(rows/2)-8, int(cols/2)-12, "[ 1 ] Hiragana "+u"\u2192"+" Romaji", curses.A_REVERSE if train_method == 0 else curses.A_NORMAL)
+        win.addstr(int(rows/2)-6, int(cols/2)-12, "[ 2 ] Katakana "+u"\u2192"+" Romaji", curses.A_REVERSE if train_method == 1 else curses.A_NORMAL)
+        win.addstr(int(rows/2)-4, int(cols/2)-12, "[ 3 ] Romaji "+u"\u2192"+" Hiragana", curses.A_REVERSE if train_method == 2 else curses.A_NORMAL)
+        win.addstr(int(rows/2)-2, int(cols/2)-12, "[ 4 ] Romaji "+u"\u2192"+" Katakana", curses.A_REVERSE if train_method == 3 else curses.A_NORMAL)
 
-    win.clear()
     draw_standard_screen(win, rows, cols)
-    train_loop(train_method)
+
+    #main loop for the trainer
+    training_list = list()
+    if train_method == 0:
+        training_list = trainer.get_random_hiragana_list()
+    elif train_method == 1:
+        training_list = trainer.get_random_hiragana_list()
+    elif train_method == 2:
+        training_list = trainer.get_random_hiragana_list()
+    elif train_method == 3:
+        training_list = trainer.get_random_hiragana_list()
+    training_length = len(training_list)
+
+    for iterations in range(training_length):
+        win.nodelay(True)
+        draw_standard_screen(win, rows, cols)
+        kana = training_list.pop()
+        win.addstr(int(rows/2), int(cols/2), kana, curses.A_NORMAL)
+        win.nodelay(False)
+        key = win.getch()
+        if key == ord("q"):
+            try:
+                quit_trainer()
+            except Exception as e:
+                return 1
+
+
 
     quit_trainer()
-
-
-
-def train_loop(train_method: int) -> None:
-    #main loop for the trainer
-    time.sleep(5)
-
 
 def handle_input() -> None:
     #helper function to handle the input
@@ -58,6 +78,7 @@ def handle_input() -> None:
 
 def draw_standard_screen(win, rows: int, cols: int) -> None:
     #draws the standard interface -> border, key assignment and title
+    win.clear()
     win.border()
     win.addstr(1, 2, "Press Q to exit", curses.A_REVERSE)
     win.addstr(2, 2, "Press E to select", curses.A_REVERSE)
@@ -73,7 +94,8 @@ def initialize_screen():
     win = curses.newwin(rows, cols, 0, 0)
     curses.curs_set(0)
     curses.noecho()
-    win.nodelay(1)
+    #curses.halfdelay(5)
+    win.nodelay(True)
     #win.timeout(1000)
     win.keypad(True)
     return win, rows, cols
